9ae58030940a3c32cc64c884df55a2fcf3ec1e9d
Souhail Bektachi <souhalshiv@gmail.com>
2024-12-20 23:23:34 +0100

    refactor: rename chatStream.go to chatStream.proto and update syntax; implement ChatStream service in client and server

--- client/main.go
+++ client/main.go
@@ -1,1 +1,60 @@
-package main;
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	pb "github.com/souhailBektachi/grpcWithGo/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	fmt.Print("Enter your username: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	username := scanner.Text()
+
+	conn, err := grpc.NewClient("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewChatStreamClient(conn)
+	stream, err := client.Chat(context.Background())
+	if err != nil {
+		log.Fatalf("failed to call Chat: %v", err)
+	}
+
+	go func() {
+		for {
+			msg, err := stream.Recv()
+			if err != nil {
+				log.Fatalf("failed to receive a message: %v", err)
+			}
+			fmt.Printf("%s :%s \n", msg.User, msg.Message)
+		}
+	}()
+
+	for scanner.Scan() {
+		msg := &pb.ChatMessage{
+			Message: scanner.Text(),
+		}
+		if err := stream.Send(&pb.ChatMessage{
+			User:    username,
+			Message: msg.Message,
+		}); err != nil {
+			log.Fatalf("failed to send a message: %v", err)
+			return
+		}
+	}
+
+}
--- server/main.go
+++ server/main.go
@@ -1,3 +1,87 @@
-package main;
+package main
 
+import (
+	"log"
+	"net"
+	"sync"
 
+	pb "github.com/souhailBektachi/grpcWithGo/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8080"
+)
+
+type ChatServer struct {
+	pb.ChatStreamServer
+	clents map[pb.ChatStream_ChatServer]chan *pb.ChatMessage
+	mu     sync.Mutex
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	chatServer := &ChatServer{
+		clents: make(map[pb.ChatStream_ChatServer]chan *pb.ChatMessage),
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterChatStreamServer(grpcServer, chatServer)
+	log.Printf("Server listening at %v", lis.Addr())
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+}
+
+func (s *ChatServer) Chat(stream pb.ChatStream_ChatServer) error {
+	clientChan := make(chan *pb.ChatMessage)
+	s.mu.Lock()
+	s.clents[stream] = clientChan
+	s.mu.Unlock()
+	defer func() {
+		s.mu.Lock()
+		delete(s.clents, stream)
+		s.mu.Unlock()
+		close(clientChan)
+		disconectingMsg := &pb.ChatMessage{Message: "Client disconnected"}
+		s.mu.Lock()
+		for _, ch := range s.clents {
+			ch <- disconectingMsg
+		}
+		s.mu.Unlock()
+	}()
+	go func() {
+		for {
+			msg, err := stream.Recv()
+			if err != nil {
+				log.Fatalf("Client disconnected: %v", err)
+				s.mu.Lock()
+				delete(s.clents, stream)
+				s.mu.Unlock()
+				return
+
+			}
+			log.Printf("Message from client: %s", msg.Message)
+			s.mu.Lock()
+			for _, ch := range s.clents {
+				ch <- msg
+			}
+			s.mu.Unlock()
+
+		}
+	}()
+
+	for msg := range clientChan {
+		if err := stream.Send(msg); err != nil {
+			log.Println("Error sending message:", err)
+			break
+		}
+	}
+	return nil
+}
